133b3431555569d9e0e4f44f1e0eef3b450fba94
Flowmikro <cloplop091@gmail.com>
2023-08-25 14:10:17 +0300

    pages sing up

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from .forms import CustomUserChangeForm
 
 
 class SingUpPageView(generic.CreateView):
+    """Регистрация"""
     form_class = CustomUserChangeForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
