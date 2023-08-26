742f32bf836b1ea8594eff9fd6fd20adeaf4c051
Flowmikro <cloplop091@gmail.com>
2023-08-26 18:37:26 +0300

    books permission

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 
 from .models import BookModel
@@ -10,8 +11,9 @@ class BookListView(ListView):
     template_name = 'books/book_list.html'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     """Полная информация о книге"""
     model = BookModel
     context_object_name = 'book_detail'
     template_name = 'books/book_detail.html'
+    login_url = 'account_login'
