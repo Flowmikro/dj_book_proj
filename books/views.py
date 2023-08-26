ba5d44f7718955fdff6e5f877904177234b63354
Flowmikro <cloplop091@gmail.com>
2023-08-26 19:17:17 +0300

    books search

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from .models import BookModel
@@ -17,3 +18,15 @@ class BookDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'book_detail'
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
+
+
+class SearchResultsListView(ListView):
+    """Поиск книги"""
+    model = BookModel
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        """Поиск по книге или автору"""
+        query = self.request.GET.get('q')
+        return BookModel.objects.filter(Q(book__icontains=query | Q(author__icontains=query)))
